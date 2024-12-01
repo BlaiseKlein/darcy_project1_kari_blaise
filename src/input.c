7169674481ecc61060f0d47d280f1f1681149e19
karisturgeon <122186990+karisturgeon@users.noreply.github.com>
2024-12-01 14:07:50 -0800

    updated game controller

--- src/input.c
+++ src/input.c
@@ -8,40 +8,39 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// SDL_GameController* setUpController(void){
-//     SDL_GameController* controller;
-//     if (SDL_Init(SDL_INIT_GAMECONTROLLER) != 0) {
-//         printf("SDL_Init Error: %s\n", SDL_GetError());
-//         return NULL;
-//         }
-//     if (SDL_NumJoysticks() == 0) {
-//         printf("No hame controllers connected\n");
-//         SDL_Quit();
-//         return NULL;
-//     }
-//     controller = SDL_GameControllerOpen(0);
-//     if (!controller) {
-//         printf("Could not open game controller: %s\n", SDL_GetError());
-//         SDL_Quit();
-//         return NULL;
-//         }
-//     return controller;
-//     }
-//
-//
-// enum move_direction getControllerInput(SDL_Event* event){
-//         if(event->type == SDL_CONTROLLERBUTTONDOWN){
-//             switch(event->cbutton.button){
-//                 case 3: return UP;
-//                 case 0: return DOWN;
-//                 case 2: return LEFT;
-//                 case 4: return RIGHT;
-//                 default: return NONE;
-//             }
-//         }
-//         return NONE;
-//
-//     }
+SDL_GameController* setUpController(void){
+    SDL_GameController* controller;
+    if (SDL_Init(SDL_INIT_GAMECONTROLLER) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return NULL;
+        }
+    if (SDL_NumJoysticks() == 0) {
+        printf("No hame controllers connected\n");
+        SDL_Quit();
+        return NULL;
+    }
+    controller = SDL_GameControllerOpen(0);
+    if (!controller) {
+        printf("Could not open game controller: %s\n", SDL_GetError());
+        SDL_Quit();
+        return NULL;
+        }
+    return controller;
+    }
+
+
+enum move_direction getControllerInput(SDL_Event* event){
+        if(event->type == SDL_CONTROLLERBUTTONDOWN){
+            switch(event->cbutton.button){
+                case UP: return UP;
+                case DOWN: return DOWN;
+                case LEFT: return LEFT;
+                case RIGHT: return RIGHT;
+                default: return NONE;
+            }
+        }
+        return NONE;
+    }
 
 enum move_direction getKeyboardInput(void)
 {
